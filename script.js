a6970a76cbad6e77c745610d477f51ebdf18d7b9
tmrln2709 <tmrln2709@gmail.com>
2023-03-06 13:21:32 +0600

    forst commit js

---
+++ script.js
@@ -0,0 +1,50 @@
+// let a = 4
+// let b = 55
+//
+// console.log(a + b)
+
+// let a = 4
+// let b = 55
+//
+// console.log(a * b)
+
+// let a = 4
+// let b = 55
+//
+// console.log(b / a)
+
+// let a = 4
+// let b = 55
+//
+// console.log(b % a)
+
+// let a = 'hello'
+// let b = ' Temirlan'
+//
+// console.log(a + b)
+
+// let a = 'hello'
+// let b = ' Temirlan'
+//
+// console.log(typeof a * b)
+
+// let a = 5
+// let b = '5'
+//
+// console.log(a * 2)
+
+// let a = 5
+// let b = '5'
+//
+// console.log(a + b)
+
+// let a = 5
+// let b = '5'
+//
+// console.log(a * b)
+
+let c = 2
+let b = undefined
+
+console.log(a = b)
+
